5e8e5002b7dcec2767a96769af71ba80febf5889
Victor Mouzinho <jvictor2985@gmail.com>
2019-11-05 19:59:17 -0300

    Obtendo localização do usuário
    Falta redirecionar a view do mapa para o mesmo

--- v2/TravelerApp/src/app/tab2/tab2.page.ts
+++ v2/TravelerApp/src/app/tab2/tab2.page.ts
@@ -1,3 +1,4 @@
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { Component } from '@angular/core';
 import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 @Component({
@@ -5,16 +6,39 @@ import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page{
+export class Tab2Page {
   map: Map;
 
   private userLatitude: any;
   private userLongitude: any;
 
-  ionViewDidEnter() { this.leafletMap(); }
+  constructor(private geolocation: Geolocation) { }
+
+  ionViewDidEnter() { 
+    this.leafletMap(); 
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+      this.userLatitude = resp.coords.latitude;
+      this.userLongitude = resp.coords.longitude;
+      console.log('lat: ' + this.userLatitude);
+      console.log('lng: ' + this.userLongitude);
+      
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+     let watch = this.geolocation.watchPosition();
+     watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+     });
+    }
+
+
   leafletMap() {
     // In setView add latLng and zoom
-    this.map = new Map('mapId').setView([-8.05389, -34.88111], 13);
+    this.map = new Map('mapId').setView([-8.05389, -34.88111], 18);
     tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     }).addTo(this.map);
   }
