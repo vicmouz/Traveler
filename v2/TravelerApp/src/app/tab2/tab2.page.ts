3354398ff8811aac9d2fbf33f668ce6a59910680
Victor Mouzinho <jvictor2985@gmail.com>
2019-11-12 20:03:18 -0300

    Início da checagem de login

--- v2/TravelerApp/src/app/tab2/tab2.page.ts
+++ v2/TravelerApp/src/app/tab2/tab2.page.ts
@@ -1,6 +1,10 @@
+import { Tab1Page } from './../tab1/tab1.page';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -11,11 +15,13 @@ export class Tab2Page {
 
   private userLatitude: any;
   private userLongitude: any;
+  private loggedIn: boolean = false;
 
-  constructor(private geolocation: Geolocation) { }
+  constructor(private geolocation: Geolocation, public alertController: AlertController, public router: Router) { }
 
-  ionViewDidEnter() { 
-    this.leafletMap(); 
+  ionViewDidEnter() {
+    this.checkPermission();
+    this.leafletMap();
     this.geolocation.getCurrentPosition().then((resp) => {
       // resp.coords.latitude
       // resp.coords.longitude
@@ -23,17 +29,17 @@ export class Tab2Page {
       this.userLongitude = resp.coords.longitude;
       console.log('lat: ' + this.userLatitude);
       console.log('lng: ' + this.userLongitude);
-      
-     }).catch((error) => {
-       console.log('Error getting location', error);
-     });
-     let watch = this.geolocation.watchPosition();
-     watch.subscribe((data) => {
+
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
       // data can be a set of coordinates, or an error (if an error occurred).
       // data.coords.latitude
       // data.coords.longitude
-     });
-    }
+    });
+  }
 
 
   leafletMap() {
@@ -42,6 +48,50 @@ export class Tab2Page {
     tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     }).addTo(this.map);
   }
+
+  checkPermission() {
+    if (this.loggedIn) {
+      console.log("Usuário logado");
+    } else {
+      this.presentAlertConfirm();
+    }
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'Não',
+          role: 'Não',
+          cssClass: 'secondary',
+          handler: () => {
+            this.loginRecusado();
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.router.navigateByUrl('tab1');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+    async loginRecusado() {
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        message: 'Você não poderá ver as recomendações personalizadas',
+        buttons: ['OK']
+      });
+  
+      await alert.present();
+    }
+  
+
   /** Remove map when we have multiple map object */
   ionViewWillLeave() {
     this.map.remove();
